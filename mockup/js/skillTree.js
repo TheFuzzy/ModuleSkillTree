1a2eb6f73d0041c5e99fd062b4aa40bfa9ef4ffa
TheFuzzy <Infestor51@gmail.com>
2013-05-14 16:15:28 +0800

    Moved W2UI to the js folder
    Added the Main html page.
    
    Reworked Modules file to support side-loading.
    
    Added skilltree.html, with the css file main.css and the js file skillTree.js, to provide module skillTree function.

---
+++ mockup/js/skillTree.js
@@ -0,0 +1,123 @@
+jsPlumb.ready(function() {
+	jsPlumb.importDefaults({
+		// default to blue at one end and green at the other
+		EndpointStyles : [{ fillStyle:'#225588' }, { fillStyle:'#558822' }],/*
+		// blue endpoints 7 px; green endpoints 11.
+		Endpoints : [ [ "Dot", {radius:7} ], [ "Dot", { radius:11 } ]],*/
+		// the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
+		// case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+		ConnectionOverlays : [
+			/*[ "Arrow", { location:0.5 } ],
+			[ "Label", { 
+				location:0.1,
+				id:"label",
+				cssClass:"aLabel"
+			}]*/
+		],
+		ConnectionsDetachable : false
+	});			
+
+	// this is the paint style for the connecting lines..
+	var connectorPaintStyle = {
+		lineWidth:2,
+		strokeStyle:"#000",
+		joinstyle:"round",
+		dashstyle: "dash"/*
+		outlineColor:"#EAEDEF",
+		outlineWidth:7*/
+	},
+	// .. and this is the hover style. 
+	connectorHoverStyle = {
+		lineWidth:2,
+		strokeStyle:"#2e2aF8",
+		dashstyle: "dash"
+	},
+	endpointHoverStyle = {fillStyle:"#2e2aF8"},
+	// the definition of source endpoints (the small blue ones)
+	sourceEndpoint = {
+		endpoint:"Blank",
+		/*paintStyle:{ fillStyle:"#225588",radius:7 },*/
+		isSource:true,
+		connector:[ "Bezier", { stub:[40, 60], gap:10, cornerRadius:5 } ],								                
+		connectorStyle:connectorPaintStyle,/*
+		hoverPaintStyle:endpointHoverStyle,
+		connectorHoverStyle:connectorHoverStyle,*/
+		dragOptions:{},
+		
+	},
+	// a source endpoint that sits at BottomCenter
+	//	bottomSource = jsPlumb.extend( { anchor:"BottomCenter" }, sourceEndpoint),
+	// the definition of target endpoints (will appear when the user drags a connection) 
+	targetEndpoint = {
+		endpoint:"Blank",					
+		/*paintStyle:{ fillStyle:"#558822",radius:11 },
+		hoverPaintStyle:endpointHoverStyle,*/
+		maxConnections:-1,
+		dropOptions:{ hoverClass:"hover", activeClass:"active" },
+		isTarget:true,			
+		
+	}/*,			
+	init = function(connection) {
+		connection.getOverlay("label").setLabel(connection.sourceId.substring(6) + "-" + connection.targetId.substring(6));
+		connection.bind("editCompleted", function(o) {
+			if (typeof console != "undefined")
+				console.log("connection edited. path is now ", o.path);
+		});
+	};*/			
+
+	var allSourceEndpoints = [], allTargetEndpoints = [];
+		_addEndpoints = function(toId, sourceAnchors, targetAnchors) {
+			for (var i = 0; i < sourceAnchors.length; i++) {
+				var sourceUUID = toId + sourceAnchors[i];
+				allSourceEndpoints.push(jsPlumb.addEndpoint(toId, sourceEndpoint, { anchor:sourceAnchors[i], uuid:sourceUUID }));						
+			}
+			for (var j = 0; j < targetAnchors.length; j++) {
+				var targetUUID = toId + targetAnchors[j];
+				allTargetEndpoints.push(jsPlumb.addEndpoint(toId, targetEndpoint, { anchor:targetAnchors[j], uuid:targetUUID }));						
+			}
+		};
+
+	_addEndpoints("CS1010box", ["BottomCenter"], ["TopCenter"]);
+	_addEndpoints("CS1101Sbox", ["BottomCenter"], ["TopCenter"]);
+	_addEndpoints("CS1020box", ["BottomCenter"], ["TopCenter"]);
+	_addEndpoints("CS2010box", ["BottomCenter"], ["TopCenter"]);
+	//_addEndpoints("box4", ["LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter"]);
+				
+	// listen for new connections; initialise them the same way we initialise the connections at startup.
+	jsPlumb.bind("jsPlumbConnection", function(connInfo, originalEvent) { 
+		init(connInfo.connection);
+	});			
+				
+	// make all the window divs draggable						
+	jsPlumb.draggable($(".moduleBox"),
+		{
+		}
+	);
+	// THIS DEMO ONLY USES getSelector FOR CONVENIENCE. Use your library's appropriate selector method!
+	//jsPlumb.draggable(jsPlumb.getSelector(".window"));
+
+
+	// connect a few up
+	jsPlumb.connect({uuids:["CS1010boxBottomCenter", "CS1020boxTopCenter"]});
+	jsPlumb.connect({uuids:["CS1101SboxBottomCenter", "CS1020boxTopCenter"]});
+	jsPlumb.connect({uuids:["CS1020boxBottomCenter", "CS2010boxTopCenter"]});
+	/*jsPlumb.connect({uuids:["window2LeftMiddle", "window4LeftMiddle"], editable:true});
+	jsPlumb.connect({uuids:["window4TopCenter", "window4RightMiddle"], editable:true});
+	jsPlumb.connect({uuids:["window3RightMiddle", "window2RightMiddle"], editable:true});
+	jsPlumb.connect({uuids:["window4BottomCenter", "window1TopCenter"], editable:true});
+	jsPlumb.connect({uuids:["window3BottomCenter", "window1BottomCenter"], editable:true});*/
+	//
+});
+$(function() {
+	$(".moduleBox").click(function(e) {
+		properties = [".moduleCode", ".moduleTitle", ".moduleDesc"];
+		for (i in properties) {
+			$("#moduleInfo").find(properties[i]).text($(this).find(properties[i]).text());
+		}
+		/*var moduleCode = $(this).find(".moduleCode").text(),
+			moduleTitle = $(this).find(".moduleTitle").text(),
+			moduleCode = $(this).find(".moduleDesc").text();
+		$("#moduleInfo").find(".moduleCode).text(*/
+		$("#moduleInfo").fadeIn("slow");
+	});
+});
