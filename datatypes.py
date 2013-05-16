b171344f66c04d0f8d58ac54bb8f72ca7a0c1491
TheFuzzy <Infestor51@gmail.com>
2013-05-16 21:28:35 +0800

    Added datatypes.py
    This file contains the Models and other classes to be stored inside the DataStore of the App Engine.
    
        - Added Student, Module and AssignedModule Models.

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+from google.appengine.ext import db
+
+# Student model
+class Student(db.Model):
+    user = db.UserProperty()
+    admin_no = db.StringProperty() # optional property?
+    modules = db.ListProperty(db.Key) # Refers to a list of keys of the Module model only
+
+class Module(db.Model):
+    code = db.StringProperty()
+    name = db.StringProperty()
+    description = db.TextProperty()
+    mc = db.IntegerProperty()
+    preclusions = db.StringListProperty() # List of module codes as Strings
+    prerequisites = db.StringListProperty() # List of module codes as Strings
+    department = db.StringProperty()
+    workload = db.StringProperty()
+
+class AssignedModule(db.Model):
+    module = db.ReferenceProperty(Module)
+    semester = db.IntegerProperty() # Which semester the student wishes to take the module in the skill tree
+    prerequisites = db.StringListProperty() # List of module codes as Strings, may amend to be keys instead
+    is_exception = db.BooleanProperty() # Whether the client should ignore pre-requisites as a "special case"
+    
